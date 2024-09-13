d39302b8d72d75c956360f22a06875468244b36a
Cerber6002 <the.b3st6002@gmail.com>
2024-09-13 11:31:49 +0600

    Create Machine Epsilon 16.cpp

---
+++ Machine Epsilon 16.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+    float e = 1;
+    int n = 0;
+
+    while (1.0 + e != 1) {
+        e /= 10;
+        n++;
+    }
+
+
+    cout << "Cycles:" << " " << n << endl;
+
+    return 0;
+}
